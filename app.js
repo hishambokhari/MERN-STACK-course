cc7ea063b9f75864154472d3124fea07c5f9c0dd
Hisham Bokhari <hish.b92@gmail.com>
2020-03-25 19:49:31 +0500

    Add: Static file loading

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ const express = require('express');
 const app = express();
 const port = 5000
 
+// serving static files
+app.use(express.static('public'))
+
+// '/' Route
 app.get('/',(req, res)=>{
-    res.send("Hello world")
+    res.sendFile(__dirname + '/index.html')
 })
 
+// '/About' route
 app.get('/about',(req, res)=>{
     res.send("This is the about page")
+})
+
+// '/Home' route
+app.get('/home',(req, res)=>{
+    res.send({msg:"walalalala"})
 })
 
 app.listen(port,()=>{
