da643fa1b99741e7be448310f4c0687f14d05040
root <hish.b92@gmail.com>
2020-03-26 16:15:06 +0500

    Add: Delete functionality

--- routes.js
+++ routes.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 
 
-const data = ['code','sleep','eat']
+var data = ['code','sleep'];
 
 
 module.exports = (app)=>{
 
-
+// get routes
     app.get('/',(req, res)=>{
         res.render('home',{wish:data})
     })
@@ -16,13 +16,27 @@ module.exports = (app)=>{
     app.get('/about',(req, res)=>{
         res.render("about")
     })
-
+    // post routes
     app.post('/sent',(req,res)=>{
         console.log(req.body.item)
         data.push(req.body.item)
         res.send(data)
     })
     
+    // delete routes
+
+    app.delete('/remove/:id',(req,res)=>{
+
+       data = data.map(item=>{
+            if(item!=req.params.id){
+                return item
+            }
+        })
+        console.log('Deleted',req.params.id)
+        res.send(data)
+    })
+
+
 }
 
 
